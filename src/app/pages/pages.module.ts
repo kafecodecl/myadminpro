8a0e87c04db2692fe3e3bfcfa4d7b2053fd7a218
Felipe Bass <felibass@gmail.com>
2019-05-28 23:22:45 -0400

    tercer commit

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+ 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { PAGES_ROUTES } from './pages.routes';
+
+// Temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 
 
 
@@ -14,7 +18,8 @@ import { PAGES_ROUTES } from './pages.routes';
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
-        PagesComponent
+        PagesComponent,
+        IncrementadorComponent
     ],
     exports: [
         DashboardComponent,
@@ -24,7 +29,8 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule
     ]
 })
 
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+  // declaraciones
+  // tslint:disable: no-inferrable-types
+  progreso1: number = 20;
+  progreso2: number = 30;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  actualizar(event: number) {
+
+  }
+
 
 }
